1602aeb8386b71b83ef9724bfc1fd39917ef5678
wheremyfiji <wher3myfiji@yandex.ru>
2023-09-29 21:57:49 +0300

    fix current week issue

--- lib/src/utils/extensions/date_time.dart
+++ lib/src/utils/extensions/date_time.dart
@@ -15,7 +15,13 @@ extension DateTimeExt on DateTime {
   }
 
   bool isEvenWeek() {
-    int currentWeekOfMonth = ((day - 1) / 7).ceil();
+    String firstDay =
+        '${toString().substring(0, 8)}01${toString().substring(10)}';
+
+    int weekDay = DateTime.parse(firstDay).weekday - 1;
+    int currentWeekOfMonth = ((day + weekDay) / 7).ceil();
+
+    //int currentWeekOfMonth = ((day - 1) / 7).ceil();
 
     return currentWeekOfMonth % 2 == 0;
   }
